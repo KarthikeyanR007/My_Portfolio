170cd50dc14e01b990f6c926fb54bfadcca6d282
karthikeyan <129775960+KarthikeyanR007@users.noreply.github.com>
2024-07-23 23:04:22 +0530

    Add files via upload

---
+++ src/assets/mywork_data.js
@@ -0,0 +1,27 @@
+import project1_img from '../assets/videopro.png'
+import project2_img from '../assets/3d-tshirt.jpeg'
+import project3_img from '../assets/movieApp.png'
+import project4_img from '../assets/project_4.svg'
+import project5_img from '../assets/project_5.svg'
+import project6_img from '../assets/project_6.svg'
+
+const mywork_data = [
+    {
+        w_no:1,
+        w_name:"Multi Streaming Application",
+        w_img:project1_img
+    },
+    {
+        w_no:2,
+        w_name:"3D T-Shirt Design Application",
+        w_img:project2_img
+    },
+    {
+        w_no:3,
+        w_name:"Movie Application",
+        w_img:project3_img
+    },
+
+]
+ 
+export default mywork_data;
---
+++ src/assets/services_data.js
@@ -0,0 +1,30 @@
+const Services_Data = [
+    {
+        s_no:"01",
+        s_name:"Web Development Services",
+        s_desc:"Real time web applications are gaining immense popularity since these types of hybrid web applications can transmit the data and information instantly from one server to another instantly."
+    },
+    {
+        s_no:"02",
+        s_name:"App Development Services",
+        s_desc:"To smoothly handle your business on mobile devices"
+    },
+    {
+        s_no:"03",
+        s_name:"Digital Marketing",
+        s_desc:"Social media marketing is a type of digital marketing that makes use of the popularity of popular social media networks to achieve marketing and branding objectives."
+    },
+    {
+        s_no:"04",
+        s_name:"Search Engine Optimization (SEO)",
+        s_desc:"SEO is the single most effective way of driving traffic to your site. Any campaign that does not include an SEO component is inadequate."
+    },
+    {
+        s_no:"05",
+        s_name:"Web Design",
+        s_desc:"Our responsive web design service is delivered keeping in mind the adaptability needs of the planning and improving the general performance of the web site "
+    },
+
+]
+ 
+export default Services_Data;
